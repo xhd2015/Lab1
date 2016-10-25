f33576e5670599d3e504f9f9eedc8ce16a77a183
Disandao <641233923@qq.com>
2016-10-25 22:13:49 +0800

    fix complish

--- JExpression/src/fulton/byperiod/atuniversity/expression/ExpressionNode.java
+++ JExpression/src/fulton/byperiod/atuniversity/expression/ExpressionNode.java
@@ -337,9 +337,9 @@ public class ExpressionNode {
 		// 排序各个项
 		additions.sort(new Comparator<String>() {
 
-			@Override
-			public int compare(String arg0, String arg1) {
-				// TODO Auto-generated method stub
+		@Override
+		public int compare(String arg0, String arg1) {
+			// TODO Auto-generated method stub
 				return arg0.compareTo(arg1);
 			}
 		});
